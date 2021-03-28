11193262511ec6413fbd7604f5da67552f458b70
Abdulla Alameri <lance.alameri@gmail.com>
2021-03-29 00:50:12 +0400

    adding colors

---
+++ colors.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void){
+
+printf("\033[0;31m");
+printf("Hello\n");
+printf("\033[0m");
+
+printf("\033[0;33m");
+printf("Hello\n");
+printf("\033[0m");
+printf("\033[0;36m");
+printf("Hello\n");
+printf("\033[0m");
+printf("\033[0;35m");
+printf("Hello\n");
+printf("\033[0m");
+printf("\033[0;34m");
+printf("Hello\n");
+
+return 0;
+}
