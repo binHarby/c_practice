c365a593e41c041094f606e98e8261c50fbd254b
Abdulla Alameri <lance.alameri@gmail.com>
2021-03-31 02:21:24 +0400

    typeDef

--- advancedTopics/simpleTypeDef.c
+++ advancedTopics/simpleTypeDef.c
@@ -2,15 +2,23 @@
 #include<stdlib.h>
 typedef int my_int;
 typedef int arr[5];
+typedef struct yes{
+	int numberOfV;
+
+
+}car; // notice this will act as the new name for the typedef struct
 int main(void){
 	my_int t =5;
 	arr anArray={5,4,3,2,1};
+	car naz_car;
+	naz_car.numberOfV=8;
 	printf("t=%d\n",t);	
 	printf("The Array Elements:\n");
 	for(int i=0;i<5;i++){
 		printf("Array[%d]=%d\n",i,anArray[i]);
 
 	}
+	printf("The Naz Car has %d cylinders \n",naz_car.numberOfV);
 
 	return 0;
 }
