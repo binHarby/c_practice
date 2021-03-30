671f7c1a234e124b0f46e376a2e1e91a8140df84
Abdulla Alameri <lance.alameri@gmail.com>
2021-03-31 02:34:29 +0400

    TypeDef2

--- advancedTopics/simpleTypeDef.c
+++ advancedTopics/simpleTypeDef.c
@@ -7,10 +7,26 @@ typedef struct yes{
 
 
 }car; // notice this will act as the new name for the typedef struct
+//OR
+struct date {
+
+	int day;
+
+};
+//now define
+typedef struct date date_type;
+typedef struct date * date_ptr;
 int main(void){
 	my_int t =5;
 	arr anArray={5,4,3,2,1};
 	car naz_car;
+	date_type someDate;
+	someDate.day=11;
+
+	date_ptr someDatePtr;
+	someDatePtr=(date_ptr)malloc(sizeof(date_type));
+	someDatePtr=&someDate;
+	someDatePtr->day=15;		
 	naz_car.numberOfV=8;
 	printf("t=%d\n",t);	
 	printf("The Array Elements:\n");
@@ -18,7 +34,7 @@ int main(void){
 		printf("Array[%d]=%d\n",i,anArray[i]);
 
 	}
-	printf("The Naz Car has %d cylinders \n",naz_car.numberOfV);
+	printf("The Naz Car has %d cylinders \nsomeDate.day=%d\n",naz_car.numberOfV,someDate.day);
 
 	return 0;
 }
