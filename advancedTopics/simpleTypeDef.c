f3ebbc1c24d8b498f400b5eef9a6aa246f2904c9
Abdulla Alameri <lance.alameri@gmail.com>
2021-03-31 02:14:50 +0400

    Type Def

--- advancedTopics/simpleTypeDef.c
+++ advancedTopics/simpleTypeDef.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 typedef int my_int;
+typedef int arr[5];
 int main(void){
-	my_int i =5;
-	printf("%d\n",i);	
+	my_int t =5;
+	arr anArray={5,4,3,2,1};
+	printf("t=%d\n",t);	
+	printf("The Array Elements:\n");
+	for(int i=0;i<5;i++){
+		printf("Array[%d]=%d\n",i,anArray[i]);
+
+	}
 
 	return 0;
 }
