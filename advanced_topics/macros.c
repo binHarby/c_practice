6de359d64e12ac3522ec477f53d4144579401cae
Abdulla Alameri <lance.alameri@gmail.com>
2021-03-31 14:00:26 +0400

    functions & datatypes as Macros

--- advanced_topics/macros.c
+++ advanced_topics/macros.c
@@ -3,8 +3,10 @@
 
 #define MACRO1 5
 #define MACRO2 6
-int main(void){
+#define Integer int
+#define square(x) ((x)*(x))
+Integer main(void){
 	printf("1st Macro is %d, 2nd Macro is %d\n", MACRO1,MACRO2);	
-
+	printf("The square of 5 is %d\n",square(5));
 	return 0;
 }
