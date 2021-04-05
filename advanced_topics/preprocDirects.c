66da485c0b8b83637e0f175e8d1778d2049087e7
Abdulla Alameri <lance.alameri@gmail.com>
2021-04-05 04:40:18 +0400

    preprocessor dirc 2

--- advanced_topics/preprocDirects.c
+++ advanced_topics/preprocDirects.c
@@ -3,13 +3,17 @@
 #undef MAX //undefines a macro
 #define MAX 20 
 #ifdef MAX   //returns true if this macro is defined
+#define MIN 0
+#endif
 #ifndef MAX //returns true if this macro is undefined
-#if //test if a compile test is true
-#else //alternative to #if
-#elif //both #else and #if
-#endif //ends preprocessor conditional 
-#error // prints error message on stderr
-#pragma // 'issues special commands to the compiler, using a standardized method 
+#define what ? //if used well through undeclared error
+#endif
+//#if //test if a compile test is true
+//#else //alternative to #if
+//#elif //both #else and #if
+//#endif //ends preprocessor conditional 
+//#error // prints error message on stderr
+//#pragma // 'issues special commands to the compiler, using a standardized method 
 /* some predefined Macros are: __DATE__,__TIME__,__FILE__,__LINE__,__STDC__
 check the print statements below for meaning these macros */
 
@@ -17,3 +21,8 @@ check the print statements below for meaning these macros */
 '#' to stringize 
 '##' to toaken pasting 
 */
+int main(void){
+	printf("Max= %d, Min= %d, error=%d\n",MAX,MIN);
+
+	return 0;
+}
