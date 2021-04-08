56809750edaed201adcc3da3a9e23c697d2b75d2
Abdulla Alameri <lance.alameri@gmail.com>
2021-04-08 16:51:18 +0400

    Redo Udemy course- practice only

---
+++ redo_udemy/hello_me.c
@@ -0,0 +1,21 @@
+#include<stdio.h>// include is a preprocessor directive 
+/* headers, macros, libraries, etc are defined before the program to be 
+preprocessed before the program starts */
+
+
+
+/* main function is the main function of any program
+as you can see, a function is first defined with a return data type.
+followed by a name an function arguments, these can be empty, void or 
+a similar syntax to this:
+int arg1, char arg2 , char **argv,char *array, char array2[],...
+
+etc 
+
+where ... makes the function a variadic function
+*/
+int main(void){
+
+	printf("Hello My name is Me\n"); // \n is a new line 
+	return 0;
+}
