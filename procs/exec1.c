5264512fd49f9436a0c78f9658fdca5f606eed3d
Abdulla Alameri <lance.alameri@gmail.com>
2021-06-12 14:55:52 +0400

    execution funct execlp #2

--- procs/exec1.c
+++ procs/exec1.c
@@ -6,9 +6,20 @@
 
 
 int main (int argc, char* argv[]){
-	execlp("ls","ls","-ahl",NULL,NULL,NULL);
-	printf("Success!\n");	
+	//when an execution command is called
+	//it  takes over an entire processes
+	//and acts like a 'return' statment
+	int pid = fork();
+	if(pid==-1) return 1;
+	if(pid==0){
+		
+		execlp("ls","ls","-ahl",NULL,NULL,NULL);
+	}else {
+		wait(NULL);
 
+		printf("Success!\n");	
+
+	}
 
 	return 0;
 }
